26c176ea7bcedb1462233cbc203f116c42a804ed
Ian Chilton <ian@ichilton.co.uk>
2013-09-09 09:48:35 +0100

    Add default recipe for installing nginx

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,20 @@
 # Cookbook Name:: nginx-web-server
 # Recipe:: default
 #
-# Copyright (C) 2013 YOUR_NAME
-# 
-# All rights reserved - Do Not Redistribute
-#
+
+# Add APT Repository:
+case node['platform_family']
+  when 'debian'
+    include_recipe 'apt'
+
+    apt_repository 'nginx' do
+      uri node['nginx-web-server']['repository_url']
+      distribution node['lsb']['codename']
+      components ['nginx']
+      key 'http://nginx.org/keys/nginx_signing.key'
+      action :add
+    end
+end
+
+# Install Package:
+package 'nginx'
