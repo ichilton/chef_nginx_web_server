66cb357cc8bd9de505e3905ddf1e77f378d9c9f2
Ian Chilton <ian@ichilton.co.uk>
2013-09-09 16:00:09 +0100

    Initial recipe work

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,13 @@
 # Attributes:: default
 #
 
-case node['platform_family']
+case node[:platform_family]
   when 'debian'
-    default['nginx-web-server']['repository_url'] = "http://nginx.org/packages/#{node['platform']}"
+    default[:nginx_web_server][:repository_url] = "http://nginx.org/packages/#{node[:platform]}"
 end
+
+default[:nginx_web_server][:config_dir] = '/etc/nginx'
+default[:nginx_web_server][:log_dir] = '/var/log/nginx'
+
+default[:nginx_web_server][:default_site][:enabled] = true
+default[:nginx_web_server][:default_site][:root_dir] = '/usr/share/nginx/html'
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,7 +9,7 @@ case node['platform_family']
     include_recipe 'apt'
 
     apt_repository 'nginx' do
-      uri node['nginx-web-server']['repository_url']
+      uri node[:nginx_web_server][:repository_url]
       distribution node['lsb']['codename']
       components ['nginx']
       key 'http://nginx.org/keys/nginx_signing.key'
@@ -19,3 +19,77 @@ end
 
 # Install Package:
 package 'nginx'
+
+directory node[:nginx_web_server][:config_dir] do
+  owner 'root'
+  group 'root'
+  mode 00755
+  recursive true
+end
+
+directory node[:nginx_web_server][:log_dir] do
+  owner 'root'
+  group 'root'
+  mode 00755
+  recursive true
+end
+
+%w(conf.d sites-available sites-enabled).each do |dir|
+  directory File.join(node[:nginx_web_server][:config_dir], dir) do
+    owner 'root'
+    group 'root'
+    mode 00755
+  end
+end
+ 
+file "#{node[:nginx_web_server][:config_dir]}/conf.d/default.conf" do
+  action :delete
+end
+
+file "#{node[:nginx_web_server][:config_dir]}/conf.d/example_ssl.conf" do
+  action :delete
+end
+
+directory node[:nginx_web_server][:default_site][:root_dir] do
+  owner 'root'
+  group 'root'
+  mode 00755
+  recursive true
+end
+
+file "#{node[:nginx_web_server][:default_site][:root_dir]}/index.html" do
+  owner 'root'
+  group 'root'
+  mode 00644
+end
+
+if Dir.glob('/etc/rc2.d/S??nginx').count > 0
+  Chef::Log.info 'NGINX DEBUG!!'
+
+  # Stop service:
+  service 'nginx' do
+    provider Chef::Provider::Service::Init::Debian
+    action [:stop]
+  end
+
+  # Disable service (disabling with chef creates /etc/rcX.d/K20nginx links):
+  execute 'disable-nginx' do
+    command "update-rc.d -f nginx remove"
+  end
+end
+
+#service 'nginx' do
+#  provider Chef::Provider::Service::Upstart
+#  supports :restart => true, :start => true, :stop => true
+#end
+
+#template "nginx-upstart.conf" do
+#  path "/etc/init/nginx.conf"
+#  source "nginx-upstart.conf.erb"
+#  owner "root"
+#  group "root"
+#  mode "0644"
+#  notifies :restart, resources(:service => "nginx")
+#end
+
+
